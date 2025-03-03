b2ff58954bfa2400f2aeeca8717bbf43ec6278aa
Godwin J. Oluwadipe <oluwadipegodwin@gmail.com>
2025-03-03 13:33:02 +0100

    removed contact file from all files, updated lingerie.html and lingery.js for product card display with slider and hovering effect. Updated Lingerie.html small card slider with more product item form easy user experience via Item.json file

--- lingery.js
+++ lingery.js
@@ -1,53 +1,251 @@
+// import imgItems from "./generalScript.js";
+
+// // Fetching data from items.json
+// const getAdsList = async () => {
+// 	const cardList = await imgItems()
+	
+
+// 	if (cardList === null) {
+// 		console.log(`Failed to loads items`)
+
+// 		return
+// 	}
+
+// 	let badgeSingleImg = document.querySelector('.badgeSingleImg')
+
+// 	badgeSingleImg.innerHTML = ` `
+// 	if (Array.isArray(cardList[0])) {
+// 		cardList[0].forEach(item => {
+// 			let swiftItem = document.createElement('div')
+// 			swiftItem.classList.add('boxSingleImg')
+// 			swiftItem.dataset.id = item.id
+// 			swiftItem.innerHTML = `
+// 							<img src="${item.image}" alt="" loading="lazy">
+// 							<div class="badgeSingleInfo">
+// 								<h3 class="h3">${item.title}</h3>
+// 								<p class="textY">
+// 									${item.description}
+// 								</p>
+// 								<a href="http://127.0.0.1:5500/images/2.jpg">Learn More</a>
+// 							</div>
+// 					`
+// 			badgeSingleImg.appendChild(swiftItem)
+// 		})
+// 	} else {
+// 		console.log("cardList is not an Array")
+// 	}
+// 	smallCardAnimation()
+// 	smallHoverEffect()
+// }
+
+// getAdsList()
+
+// // SWIFT ITEM CARD ANIMATION **********
+// let intervalId;
+// let isPaused = false;
+// const smallCardAnimation = () => {
+// 	let smallCardIndex = 0;
+// 	const showSmallCard = () => {
+// 		let boxSingleImg = document.querySelectorAll('.boxSingleImg')
+
+
+// 		let countDownCard = boxSingleImg.length
+
+// 		for (let v = 0; v < countDownCard; v++) {
+// 			boxSingleImg[v].style.display = 'none';
+// 		}
+
+// 		smallCardIndex++
+
+// 		if (smallCardIndex > countDownCard) {
+// 			smallCardIndex = 1
+// 		}
+
+// 		boxSingleImg[smallCardIndex - 1].style.display = 'flex'
+// 	}
+
+// 	return showSmallCard
+// }
+// const holdIt = smallCardAnimation()
+
+// // HANDLING MOUSEOVER OPERATION******
+// const smallHoverEffect = () => {
+// 	let boxSingleImg = document.querySelectorAll('.boxSingleImg');
+	
+// 	boxSingleImg.forEach(cardImg => {
+// 		cardImg.addEventListener('mouseover',  () => {
+//         clearInterval(intervalId);
+// 		isPaused = true;
+// 		});
+		
+// 		cardImg.addEventListener('mouseleave', () => {
+//         isPaused = false;
+//         startInterval();
+// 		});
+// 	});
+// }
+
+
+// // SLIDER TIMER****
+// const startInterval = () => {
+// 	intervalId = setInterval(holdIt, 1500)
+
+// }
+
+// startInterval()
+
+// //
+
+
 import imgItems from "./generalScript.js";
 
 // Fetching data from items.json
 const getAdsList = async () => {
 	const cardList = await imgItems()
 	
-
 	if (cardList === null) {
 		console.log(`Failed to loads items`)
 
-		return
+		return null
 	}
 
-	let badgeSingleImg = document.querySelector('.badgeSingleImg')
-
-	badgeSingleImg.innerHTML = ` `
-	if (Array.isArray(cardList[0])) {
-		cardList[0].forEach(item => {
-			let swiftItem = document.createElement('div')
-			swiftItem.classList.add('boxSingleImg')
-			swiftItem.dataset.id = item.id
-			swiftItem.innerHTML = `
-							<img src="${item.image}" alt="" loading="lazy">
-							<div class="badgeSingleInfo">
-								<h3 class="h3">${item.title}</h3>
-								<p class="textY">
-									${item.description} 
-								</p>
-								<a href="http://127.0.0.1:5500/images/2.jpg">Learn More</a>
-							</div>
-					`
-			badgeSingleImg.appendChild(swiftItem)
-		})
-	} else {
-		console.log("cardList is not an Array")
-	}
-	swiftAnimation()
-	hoverEffect()
+	return  cardList
 }
 
-getAdsList()
 
-// SWIFT ITEM CARD ANIMATION **********
-let intervalId;
+
+getAdsList().then(result => {
+	if (result !== null) {
+		console.log(result)
+	}
+
+	// Big Card Slider for lingerie.html
+	const topBoardItemsDisplay = () => {
+		console.log(result[2])
+		let badgeInfo = document.querySelector('.badgeInfo') 
+		
+		badgeInfo.innerHTML = ''
+
+		if (Array.isArray(result[2])) {
+			result[2].forEach(currentItem => {
+				let newCard = document.createElement('div')
+				newCard.classList.add('infoHolder')
+				newCard.dataset.id = currentItem.id
+				newCard.innerHTML = `
+						<div class="lingerImg">
+							<img src="${currentItem.image}" alt="" loading="lazy">
+				   		</div>
+						<div class="lingerText">
+							<h3>${currentItem.title}</h3>
+							<p class="inText">
+								${currentItem.productInfo}
+							</p>
+							<div class="actionBtns">
+								<i class="fas fa-heart"></i>
+								<i class="fas fa-share-alt"></i>
+								<button class="needThis">I need This</button>
+							</div>
+						</div>
+				`
+				badgeInfo.appendChild(newCard)
+			})
+		} else {
+			console.log("result is not an Array!")
+		}
+
+		BigCardAnimation()
+		BigHoverEffect()
+	}
+
+	topBoardItemsDisplay()
+
+	// Small Slider Display for lingerie.html
+	const smallSliderCard = () => {
+		let badgeSingleImg = document.querySelector('.badgeSingleImg')
+
+		badgeSingleImg.innerHTML = ` `
+		if (Array.isArray(result[0])) {
+			result[0].forEach(item => {
+				let swiftItem = document.createElement('div')
+				swiftItem.classList.add('boxSingleImg')
+				swiftItem.dataset.id = item.id
+				swiftItem.innerHTML = `
+								<img src="${item.image}" alt="" loading="lazy">
+								<div class="badgeSingleInfo">
+									<h3 class="h3">${item.title}</h3>
+									<p class="textY">
+										${item.description} 
+									</p>
+									<a href="http://127.0.0.1:5500/images/2.jpg">Learn More</a>
+								</div>
+						`
+				badgeSingleImg.appendChild(swiftItem)
+			})
+		} else {
+			console.log("cardList is not an Array")
+		}
+		smallCardAnimation()
+		smallHoverEffect()
+	}
+	smallSliderCard()
+})
+
+// Big Card Animation********
+let BigCardIntervalId
+const BigCardAnimation = () => {
+	let BigCardIndex = 0
+	const BigCardsCount = () => {
+		let infoHolder = document.querySelectorAll('.infoHolder')
+
+		let countBigCard = infoHolder.length
+
+		for (let B = 0; B < countBigCard; B++) {
+			infoHolder[B].style.display = 'none';
+		}
+
+		BigCardIndex++
+
+		if (BigCardIndex > countBigCard) {
+			BigCardIndex = 1
+		}
+
+		infoHolder[BigCardIndex - 1].style.display = 'flex'
+	}
+
+	return BigCardsCount
+}
+
+const BigHoldIt = BigCardAnimation()
+
+const BigCardTimerInterval = () => {
+	BigCardIntervalId = setInterval(BigHoldIt, 1000)
+}
+BigCardTimerInterval()
+
+const BigHoverEffect = () => {
+	let infoHolder = document.querySelectorAll('.infoHolder')
+	console.log(infoHolder)
+
+	infoHolder.forEach(BigCardImg => {
+		BigCardImg.addEventListener('mouseover', () => {
+			clearInterval(BigCardIntervalId)
+			isPaused = true;
+		})
+
+		BigCardImg.addEventListener('mouseleave', () => {
+			isPaused = false;
+			BigCardTimerInterval()
+		})
+	})
+}
+
+// Small Cards ANIMATION **********
+let smallCardIntervalId;
 let isPaused = false;
-const swiftAnimation = () => {
-	let swiftCardIndex = 0;
-	const showSwiftCard = () => {
+const smallCardAnimation = () => {
+	let smallCardIndex = 0;
+	const showSmallCard = () => {
 		let boxSingleImg = document.querySelectorAll('.boxSingleImg')
-
 
 		let countDownCard = boxSingleImg.length
 
@@ -55,26 +253,26 @@ const swiftAnimation = () => {
 			boxSingleImg[v].style.display = 'none';
 		}
 
-		swiftCardIndex++
+		smallCardIndex++
 
-		if (swiftCardIndex > countDownCard) {
-			swiftCardIndex = 1
+		if (smallCardIndex > countDownCard) {
+			smallCardIndex = 1
 		}
 
-		boxSingleImg[swiftCardIndex - 1].style.display = 'flex'
+		boxSingleImg[smallCardIndex - 1].style.display = 'flex'
 	}
 
-	return showSwiftCard
+	return showSmallCard
 }
-const holdIt = swiftAnimation()
+const holdIt = smallCardAnimation()
 
 // HANDLING MOUSEOVER OPERATION******
-const hoverEffect = () => {
+const smallHoverEffect = () => {
 	let boxSingleImg = document.querySelectorAll('.boxSingleImg');
 	
 	boxSingleImg.forEach(cardImg => {
 		cardImg.addEventListener('mouseover',  () => {
-        clearInterval(intervalId);
+        clearInterval(smallCardIntervalId);
 		isPaused = true;
 		});
 		
@@ -88,10 +286,6 @@ const hoverEffect = () => {
 
 // SLIDER TIMER****
 const startInterval = () => {
-	intervalId = setInterval(holdIt, 1500)
-
+	smallCardIntervalId = setInterval(holdIt, 1500)
 }
-
 startInterval()
-
-// 
